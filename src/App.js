f4cfb7ced2140d3417107db27d619c09833e6248
Osaro20 <oussamaouldrhila292@gmail.com>
2023-12-09 22:27:16 +0100

    starting the home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import NavBar from "./NavBar";
+import Home from "./Home";
 
 function App() {
   return (
     <div className="App">
        <NavBar></NavBar>
        <div className="content">
-       
+            <Home></Home>
        </div>
     </div>
   );
---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import img1 from "./Home-img1.png"
+
+const Home = () => {
+    return ( 
+        <div className="firstpg">
+            <img src={img1} />
+        </div>
+     );
+}
+ 
+export default Home;
