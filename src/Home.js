60014c31ba1f4856dfc417c37fca8a37b680bdec
Osaro20 <oussamaouldrhila292@gmail.com>
2023-12-10 11:54:00 +0100

    finishing the contribure part of the home page

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,10 @@ import img1 from "./Home-img1.png"
 const Home = () => {
     return ( 
         <div className="firstpg">
+            <h1>Connecting Compassion for Rapid Relief.</h1>
+            <h2>Unifying Hearts, Empowering Lives</h2>
             <img src={img1} />
+            <button>CONTRIBUTE</button>
         </div>
      );
 }
